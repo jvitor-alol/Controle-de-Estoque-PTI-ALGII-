67389e71158114ae1b0e775567448893d4d5c465
jvitor-alol <jvitor.alol@protonmail.com>
2023-05-08 17:27:27 -0300

    Classe Metodos Criada

---
+++ controleEstoque/Metodos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleEstoque
+{
+    public class Metodos
+    {
+        public static void printMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Controle de estoque de DVDs da Blockbuster\n\nSelecione uma opção:");
+            Console.WriteLine("[1] Novo              | [4] Entrada Estoque");
+            Console.WriteLine("[2] Listar Produtos   | [5] Saída Estoque  ");
+            Console.WriteLine("[3] Remover Produtos  | [0] Sair           ");
+        }
+
+        public static void Novo(List<Filme> estoqueFilmes)
+        {
+
+        }
+        public static void Listar(List<Filme> estoqueFilmes)
+        {
+
+        }
+        public static void Remover(List<Filme> estoqueFilmes)
+        {
+
+        }
+        public static void Entrada(List<Filme> estoqueFilmes)
+        {
+
+        }
+        public static void Saida(List<Filme> estoqueFilmes)
+        {
+
+        }
+    }
+}
