392492e9f9fdede7bac5fe7322745adb562e56a6
jvitor-alol <jvitor.alol@protonmail.com>
2023-05-08 17:27:03 -0300

    Classe Filme criada

---
+++ controleEstoque/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleEstoque
+{
+    public class Filme
+    {
+        public string? Nome { get; set; }
+        public int Quantidade { get; set;}
+        public double Preco { get; set; }
+        public int Ano { get; set; }
+        public string? Classificacao { get; set;}
+        public string? Diretor { get; set;}
+    }
+}
