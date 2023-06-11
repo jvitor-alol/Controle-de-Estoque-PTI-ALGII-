560f91dd11a8b7d6c4c73b1b177840af0ca66299
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2023-06-11 18:17:37 -0300

    add-persistent-memory(testing)

--- controleEstoque/Program.cs
+++ controleEstoque/Program.cs
@@ -3,20 +3,39 @@
     internal class Program
     {
         enum Option { Sair, Novo, Listar, Remover, Entrada, Saida }
+        public static int idAdicional = 1;
 
         static void Main(string[] args)
         {
             bool endProgram = false;
             Option select;
-            int idAdicional = 1;
-
+            int opcao = 0; //temporario para testes com leitura e escrita de arquivos persistentes
+            string caminhoAbsoluto = "~/Development/Controle-de-Estoque-PTI-ALGII-/controleEstoque/estoquePersist.txt";
             List<Filme> estoqueFilmes = new List<Filme>();
+
+            //escolher usar base de dados ou não
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao){
+                case 0:
+                    //usa arquivo de texto
+                    Console.WriteLine("Recuperando base de dados...");
+                    lerEstoque(estoqueFilmes, caminhoAbsoluto, ref idAdicional);
+                    break;
+                case 1:
+                    Console.WriteLine("Recriando base de dados...");
+                    break;
+                default:
+                    Console.WriteLine("Opção invalida!!!");
+                    endProgram = true;
+                    break;
+            }
 
             do
             {
                 Metodos.printMenu();
                 Console.Write("\nOpção: ");
-                select = (Option)Convert.ToInt32(Console.ReadLine());
+                select = (Option)Convert.ToInt32(Console.ReadLine()); //testar depois sem o cast, creio que funcione
 
                 switch (select)
                 {
@@ -45,8 +64,29 @@
                 }
             } while (!endProgram);
 
-            Console.WriteLine("\n###### Fechando o programa... ######");
+            Console.WriteLine("\n######## Salvando os dados... ########");
+            salvarEstoque(estoqueFilmes, caminhoAbsoluto, idAdicional);
+
+            Console.WriteLine("\n####### Fechando o programa... #######");
             Console.ReadKey();
+        }
+
+        private static void salvarEstoque(List<Filme> estoqueFilmes, string caminhoAbsoluto, int idAdicional)
+        {
+            //salvando base de dados no arquivo de texto
+        }
+
+        private static void lerEstoque(List<Filme> estoqueFilmes, string caminhoAbsoluto, ref int idAdicional)
+        {
+            //lendo arquivos do texto
+            bool problemaArquivo = false;
+            try{
+                StreamReader sr = new StreamReader(caminhoAbsoluto);
+
+            }
+            catch (Exception e){
+                
+            }
         }
     }
 }
